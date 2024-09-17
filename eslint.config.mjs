e2343c54284281ef4d6b70555c1a08e5092e6a6e
Win-TS <winthanatpong@gmail.com>
2024-09-17 12:13:06 +0700

    chore: add husky, eslint, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import withNuxt from './.nuxt/eslint.config.mjs';
+import vue from 'eslint-plugin-vue';
+import nuxt from 'eslint-plugin-nuxt';
+
+export default withNuxt(
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    plugins: [vue, nuxt],
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-explicit-any': [
+        'off',
+        { ignoreRestArgs: true },
+      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      'vue/no-v-html': 'off',
+      'vue/multi-word-component-names': 'off',
+      'max-len': 'off',
+      'vue/first-attribute-linebreak': 'off',
+    },
+  },
+);
