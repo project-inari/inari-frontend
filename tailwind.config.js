9e3bd08df27c586d988553ef852cda51668b6ca9
win-ts <winthanatpong@gmail.com>
2024-10-05 15:12:15 +0700

    chore: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './components/**/*.{vue,js,ts,jsx,tsx}',
+        './pages/**/*.{vue,js,ts,jsx,tsx}',
+        './layouts/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
