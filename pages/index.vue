15213544319070b7f28f6b99b3ddee522b9446b2
win-ts <winthanatpong@gmail.com>
2024-10-07 13:53:52 +0700

    style: setup fonts in tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h1>Home Page</h1>
+        <h1 class="font-dmsans">Home Page</h1>
+        <h1 class="font-prompt">หน้าหลัก</h1>
+        <h1 class="font-sarabun">หน้าหลัก</h1>
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './components/**/*.{vue,js,ts,jsx,tsx}',
-        './pages/**/*.{vue,js,ts,jsx,tsx}',
-        './layouts/**/*.{vue,js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {},
+  content: [
+    './components/**/*.{vue,js,ts,jsx,tsx}',
+    './pages/**/*.{vue,js,ts,jsx,tsx}',
+    './layouts/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dmsans': ['DM Sans', 'sans-serif'],
+        'prompt': ['Prompt', 'sans-serif'],
+        'sarabun': ['Sarabun', 'sans-serif'],
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 };
