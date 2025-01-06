abdbe09e905cecfbe19feb3f3e923958f04b86d1
Thanatpong [Win] Songjitsomboon <winthanatpong@gmail.com>
2025-01-06 20:24:42 +0700

    fix: subdomain host parts check for deployment

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -4,13 +4,13 @@ export default defineEventHandler(event => {
     const env = useEnvironment();
     const hostParts = getRequestURL(event).host.split('.');
     if (env === 'ALPHA' || env === 'UAT') {
+        if (hostParts.length === 3) return;
+    } else if (env === 'PROD') {
         if (hostParts.length === 2) return;
     } else {
         if (hostParts.length === 1) return;
     }
     const subdomain = hostParts[0];
 
-    // eslint-disable-next-line no-console
-    console.log('url', getRequestURL(event).host);
     event.context.subdomain = subdomain;
 });
